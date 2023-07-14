015c753c483e9cac2cb78b93f443e1c917c4dc72
Akash <akashdogra171@gmail.com>
2023-07-14 16:59:33 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+import CurrencyInput from "./CurrencyInput";
+import axios from "axios";
+import "./app.css";
+
+const App = () => {
+  const [amount1, setAmount1] = useState(1);
+  const [amount2, setAmount2] = useState(1);
+  const [currency1, setCurrency1] = useState("USD");
+  const [currency2, setCurrency2] = useState("INR");
+  const [rates, setRates] = useState([]);
+
+  const getRates = () => {
+    axios
+      .get(
+        "http://data.fixer.io/api/latest?access_key=3577ec41740bd590e72bafa71befb738&format=1"
+      )
+      .then((response) => {
+        setRates(response.data.rates);
+      });
+  };
+
+  useEffect(() => {
+    getRates();
+  }, []);
+
+  useEffect(() => {
+    if (!!rates) {
+      handleAmount1Change(1);
+    }
+  }, [rates]);
+
+  function format(number) {
+    return number.toFixed(4);
+  }
+
+  function handleAmount1Change(amount1) {
+    setAmount2(format((amount1 * rates[currency2]) / rates[currency1]));
+    setAmount1(amount1);
+  }
+
+  function handleCurrency1Change(currency1) {
+    setAmount2(format((amount1 * rates[currency2]) / rates[currency1]));
+    setCurrency1(currency1);
+  }
+
+  function handleAmount2Change(amount2) {
+    setAmount1(format((amount2 * rates[currency1]) / rates[currency2]));
+    setAmount2(amount2);
+  }
+
+  function handleCurrency2Change(currency2) {
+    setAmount1(format((amount2 * rates[currency1]) / rates[currency2]));
+    setCurrency2(currency2);
+  }
+
+  return (
+    <div>
+      <h1>Currency Converter</h1>
+      <CurrencyInput
+        onAmountChange={handleAmount1Change}
+        onCurrencyChange={handleCurrency1Change}
+        currencies={Object.keys(rates)}
+        amount={amount1}
+        currency={currency1}
+      />
+      <CurrencyInput
+        onAmountChange={handleAmount2Change}
+        onCurrencyChange={handleCurrency2Change}
+        currencies={Object.keys(rates)}
+        amount={amount2}
+        currency={currency2}
+      />
+    </div>
+  );
+};
+
+export default App;
